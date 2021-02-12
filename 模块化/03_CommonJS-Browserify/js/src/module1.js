584e74d99ea44d3026b5de21d7578759c5dc415a
pandaXiong01 <1159508433@qq.com>
2021-02-12 15:19:39 +0800

    更新note

--- 模块化/03_CommonJS-Browserify/js/src/module1.js
+++
@@ -1,8 +0,0 @@
-/**
- * 使用module.exports = value向外暴露一个对象
- */
-module.exports = {
-  foo() {
-    console.log('moudle1 foo()')
-  }
-}
--- 模块化/03_CommonJS-Browserify/js/src/module2.js
+++
@@ -1,6 +0,0 @@
-/**
- * 使用module.exports = value向外暴露一个函数
- */
-module.exports = function () {
-  console.log('module2()')
-}
